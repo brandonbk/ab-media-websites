d477b3500c6112ba4e390c711f646fc31f07995f
Brian Miller <brian@parameter1.com>
2022-09-06 12:14:28 -0500

    Add fake leaders config to ab for testing
    for now point it at /directory alias & no header images.

---
+++ sites/athleticbusiness.com/config/leaders.js
@@ -0,0 +1,9 @@
+module.exports = {
+  enabled: process.env.LEADERS_ENABLED || true,
+  title: 'Leaders in Athletics',
+  alias: process.env.LEADERS_ALIAS || 'directory',
+  // header: {
+  //   imgSrc: process.env.LEADERS_LOGO || 'https://img.packworld.com/files/base/pmmi/all/leaders/pw-2022.svg?auto=format,compress&h=90',
+  //   imgSrcset: process.env.LEADERS_LOGO || 'https://img.packworld.com/files/base/pmmi/all/leaders/pw-2022.svg?auto=format,compress&h=90&dpr=2 2x',
+  // },
+};
