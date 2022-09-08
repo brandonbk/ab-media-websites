d375d9738abc3d683532f5de82223967f15d9a93
Brian Miller <brian@parameter1.com>
2022-09-08 11:35:59 -0500

    Set AB’s leaders config

--- sites/athleticbusiness.com/config/leaders.js
+++ sites/athleticbusiness.com/config/leaders.js
@@ -1,9 +1,11 @@
+const enabled = (process.env.LEADERS_ENABLED === 'true' || process.env.LEADERS_ENABLED === true) || false;
 module.exports = {
-  enabled: process.env.LEADERS_ENABLED || true,
+  enabled,
   title: 'Leaders in Athletics',
-  alias: process.env.LEADERS_ALIAS || 'directory',
-  // header: {
-  //   imgSrc: process.env.LEADERS_LOGO || 'https://img.packworld.com/files/base/pmmi/all/leaders/pw-2022.svg?auto=format,compress&h=90',
-  //   imgSrcset: process.env.LEADERS_LOGO || 'https://img.packworld.com/files/base/pmmi/all/leaders/pw-2022.svg?auto=format,compress&h=90&dpr=2 2x',
-  // },
+  calloutValue: 'Leaders in Athletics',
+  alias: process.env.LEADERS_ALIAS || 'directory/aquatic-facility-components/safety',
+  header: {
+    imgSrc: process.env.LEADERS_LOGO || 'https://img.athleticbusiness.com/files/base/abmedia/all/image/static/leaders-2022.png?auto=format,compress&h=90',
+    imgSrcset: process.env.LEADERS_LOGO || 'https://img.athleticbusiness.com/files/base/abmedia/all/image/static/leaders-2022.png?auto=format,compress&h=90&dpr=2 2x',
+  },
 };
