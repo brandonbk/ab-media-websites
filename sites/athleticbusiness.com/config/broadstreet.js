25546aba8e04f2b22ce12e6329ff9b948fdfd79d
Brian Miller <brian@parameter1.com>
2022-09-23 12:51:43 -0500

    Add zone alias sizeMapping support to broadstreet
    Update the broadstreet ad init & zone calls to handle dynamicly set the zone id based on the sizeMapping array now set in the broadstreet config.  This allows you to call a single zone alias but based on the screen size at request time set the corisponding zoneId.

--- sites/athleticbusiness.com/config/broadstreet.js
+++ sites/athleticbusiness.com/config/broadstreet.js
@@ -5,11 +5,37 @@ const config = new BroadstreetConfiguration(7652);
 module.exports = ({
   ...config,
   enable: true,
-  useAltZone: 800,
   zones: {
-    billboard: 99650,
-    leaderboard: 99648,
-    rotation: 99649,
-    stickyLeaderboard: 99648,
+    billboard: {
+      zoneIdSizeMapping: [
+        ['(min-width: 980px)', 99650],
+        ['(min-width: 750px) and (max-width: 980px)', 99655],
+        ['(min-width: 300px) and (max-width: 750px)', 99655],
+      ],
+    },
+    leaderboard: {
+      zoneIdSizeMapping: [
+        ['(min-width: 980px)', 99648],
+        ['(min-width: 750px) and (max-width: 980px)', 99656],
+        ['(min-width: 300px) and (max-width: 750px)', 99656],
+      ],
+    },
+    rotation: {
+      zoneIdSizeMapping: [
+        ['(min-width: 980px)', 99649],
+        ['(min-width: 750px) and (max-width: 980px)', 99657],
+        ['(min-width: 300px) and (max-width: 750px)', 99657],
+      ],
+    },
+    contentDesktopRotation: {
+      zoneIdSizeMapping: [
+        ['(min-width: 750px)', 99649],
+      ],
+    },
+    contentMobileRotation: {
+      zoneIdSizeMapping: [
+        ['', 99657],
+      ],
+    },
   },
 });
