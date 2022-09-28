2e16fbbadfbfe583f569c4c69b87f0b31ad70584
Brian Miller <brian@parameter1.com>
2022-09-28 13:35:28 -0500

    Update billboard/leader board component for BAM
    Remove old GAM refs and logic.

--- packages/global/browser/billboard-cookie.vue
+++ packages/global/browser/billboard-cookie.vue
@@ -6,14 +6,6 @@ import cookies from 'js-cookie';
 
 export default {
   props: {
-    adManagerType: {
-      type: String,
-      default: 'gam',
-    },
-    adUnitPath: {
-      type: String,
-      required: true,
-    },
     cookieName: {
       type: String,
       required: true,
@@ -28,20 +20,7 @@ export default {
     },
   },
   created() {
-    const { adManagerType } = this;
-    if (adManagerType === 'gam') {
-      const { googletag } = window;
-      if (!googletag) return;
-      googletag.cmd.push(() => {
-        googletag.pubads().addEventListener('slotOnload', (event) => {
-          if (event.slot.getAdUnitPath() === this.adUnitPath) {
-            cookies.set(this.cookieName, this.cookieValue, { expires: this.expires });
-          }
-        });
-      });
-    } else if (adManagerType === 'bam') {
-      cookies.set(this.cookieName, this.cookieValue, { expires: this.expires });
-    }
+    cookies.set(this.cookieName, this.cookieValue, { expires: this.expires });
   },
 };
 </script>
