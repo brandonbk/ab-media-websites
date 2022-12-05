c3b7f988c54bd7ca1b7dec33be36f23ed9128d93
Jake Collins <shinsina@att.net>
2022-12-05 13:59:38 -0600

    Reconfigure Leaders header, Aqua

--- sites/aquamagazine.com/config/leaders.js
+++ sites/aquamagazine.com/config/leaders.js
@@ -2,11 +2,10 @@ const enabled = (process.env.LEADERS_ENABLED === 'true' || process.env.LEADERS_E
 module.exports = {
   enabled,
   title: 'Leaders in Aqua',
-  calloutValue: 'Leaders in Aqua',
+  calloutValue: 'Leaders in Pool & Spa',
   alias: process.env.LEADERS_ALIAS || 'leaders/2022',
   header: {
-    imgSrc: process.env.LEADERS_LOGO || 'https://img.aquamagazine.com/files/base/abmedia/all/image/static/Leaders_Header_Aqua.jpeg?auto=format,compress&h=90',
-    imgSrcset: process.env.LEADERS_LOGO || 'https://img.aquamagazine.com/files/base/abmedia/all/image/static/Leaders_Header_Aqua.jpeg?auto=format,compress&h=90&dpr=2 2x',
+    imgSrc: process.env.LEADERS_LOGO || 'https://img.aquamagazine.com/files/base/abmedia/all/image/static/leaders-2022.png?auto=format,compress&h=90',
+    imgSrcset: process.env.LEADERS_LOGO || 'https://img.aquamagazine.com/files/base/abmedia/all/image/static/leaders-2022.png?auto=format,compress&h=90&dpr=2 2x',
   },
-  displayCallout: false,
 };
