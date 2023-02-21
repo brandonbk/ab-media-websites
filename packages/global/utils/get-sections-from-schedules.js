338c0fb14448c3a2d60c6c9a5fa604018fc02b7e
Brian Miller <brian@parameter1.com>
2023-02-21 13:27:28 -0600

    fix lint error

--- packages/global/utils/get-sections-from-schedules.js
+++ packages/global/utils/get-sections-from-schedules.js
@@ -1,6 +1,9 @@
 module.exports = (schedules = [], rootAlias = 'directory') => {
   const sections = schedules
-    .filter((schedule) => schedule.section.alias.startsWith(rootAlias) && schedule.section.alias !== rootAlias)
+    .filter((schedule) => {
+      const { alias } = schedule.section;
+      return alias.startsWith(rootAlias) && alias !== rootAlias;
+    })
     .map((schedule) => schedule.section)
     .sort((a, b) => a.name.localeCompare(b.name));
   return sections;
