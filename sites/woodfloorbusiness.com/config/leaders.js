8b92d453f28795e61bda0dd4636a71d2cd735961
Brian Miller <brian@parameter1.com>
2023-02-21 13:11:21 -0600

    add new env for LeadersListedInFind & LeadersListedInReplace
    This will be used in the new leaders Listed In block

--- sites/woodfloorbusiness.com/config/leaders.js
+++ sites/woodfloorbusiness.com/config/leaders.js
@@ -3,6 +3,8 @@ module.exports = {
   enabled,
   title: 'Leaders in Wood Floor Business',
   calloutValue: 'Leaders in Wood Floor Business',
+  listedInFind: process.envLEADERS_LISTED_IN_FULLNAME_FIND || 'Leaders > 2022 > ',
+  listedInReplace: process.envLEADERS_LISTED_IN_FULLNAME_REPLACE || '',
   alias: process.env.LEADERS_ALIAS || 'leaders/2022',
   header: {
     imgSrc: process.env.LEADERS_LOGO || 'https://img.woodfloorbusiness.com/files/base/abmedia/all/image/static/wfb-leaders.jpg?auto=format,compress&h=180',
