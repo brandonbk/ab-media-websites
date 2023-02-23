a991e00612771b078c5986d81d2677506880d0a9
Jacob Bare <jacob@parameter1.com>
2023-02-23 12:33:10 -0600

    Use common eslint configs from core and config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,1 @@
-module.exports = {
-  extends: 'airbnb-base',
-  plugins: [
-    'import'
-  ],
-  settings: {
-    'import/resolver': {
-      node: {
-        extensions: ['.js', '.marko'],
-      },
-    },
-  },
-  rules: {
-    'import/extensions': ['error', 'ignorePackages', { js: 'never', marko: 'never', json: 'always' } ],
-  },
-};
+module.exports = require('@parameter1/base-cms-eslint/eslintrc.server');
