7b6a1ca36f6292a2f3d3ccebc07595dce7e9ea3a
Brian Miller <brian@parameter1.com>
2023-04-13 08:31:56 -0500

    Add check for queryParam or cookie presence for bypassing gate
    Ensure that if there is an oly_eny_id set in the queryParams or within cookies that it will bypass the content gating logic.

--- packages/global/middleware/content-gating.js
+++ packages/global/middleware/content-gating.js
@@ -1,13 +1,20 @@
 const { get, getAsArray } = require('@parameter1/base-cms-object-path');
 const contentGatingHandler = require('@parameter1/base-cms-marko-web-theme-monorail/middleware/content-gating');
+const olyticsCookie = require('@parameter1/base-cms-marko-web-omeda/olytics/customer-cookie');
 
 module.exports = (app, options) => (req, res, next) => {
-  const hasCookie = Boolean(get(req, 'cookies.oly_enc_id'));
+  // Check for presence of oly_enc_id being passed in or being set within cookies
+  // Bypass gating of content if this is present.
+  const { oly_enc_id: id } = req.query;
+  const incomingEncId = olyticsCookie.clean(id);
+  const hasReqCookie = Boolean(get(req, 'cookies.oly_enc_id'));
+  const hasOlyEncId = Boolean(incomingEncId || hasReqCookie);
   // Apply site level and org level content gating rules
   const contentTypesToGateByDefault = getAsArray(options, 'siteConfig.contentTypesToGateByDefault');
   const contentGating = ({ content }) => {
-    if (hasCookie) return false;
-    // ensure content exists or return false
+    // This will bypass all content gating if a oly_enc_id cookie is set!
+    if (hasOlyEncId) return false;
+    // Ensure content exists or return false
     if (!content) return false;
     // Force gating by reg base on site's for contentGatingByType by content type
     if (contentTypesToGateByDefault.includes(content.type)) {
