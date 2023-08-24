850a6f71c432de813f26f88982964f6766d2a043
Jessica Wade <jessica.wade@parameter1.com>
2023-08-24 08:11:35 -0500

    Remove content meter from AB

--- sites/athleticbusiness.com/config/site.js
+++ sites/athleticbusiness.com/config/site.js
@@ -1,7 +1,7 @@
 const digitalEdition = require('./digital-edition');
 const navigation = require('./navigation');
 const bam = require('./bam');
-const contentMeter = require('./content-meter');
+// const contentMeter = require('./content-meter');
 const nativeX = require('./native-x');
 const magazine = require('./magazine');
 const newsletter = require('./newsletter');
@@ -17,12 +17,12 @@ const leaders = require('./leaders');
 module.exports = {
   navigation,
   bam,
-  contentMeter,
+  // contentMeter,
   leaders,
   nativeX,
   magazine,
   contentTypesToGateByDefault: [
-    'article',
+    // 'article',
   ],
   // temp fix till we update base-cms package
   // Dep Upgrades after https://github.com/parameter1/base-cms/pull/364
