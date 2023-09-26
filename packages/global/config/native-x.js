f3fc46d110a970210e58337cc44053ae46584920
Brandon Krigbaum <brandonbk@gmail.com>
2023-09-26 12:00:35 -0500

    Add tenantKey to native-x config

--- packages/global/config/native-x.js
+++ packages/global/config/native-x.js
@@ -3,4 +3,4 @@ const NativeXConfiguration = require('@parameter1/base-cms-marko-web-native-x/co
 module.exports = ({
   uri = 'https://delivery.mindfulcms.com/abmedia/default/compat/native-website',
   enabled = false,
-} = {}) => new NativeXConfiguration(uri, { enabled });
+} = {}) => new NativeXConfiguration(uri, { enabled, tenantKey: 'abmedia' });
