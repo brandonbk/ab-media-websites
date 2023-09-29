c68ed6b7a2605b3f07c6792d9925ad97f6615215
Brian Miller <brian@parameter1.com>
2023-09-29 09:07:02 -0500

    update key to primary_business vs primary_role

--- sites/aquamagazine.com/config/identity-x.js
+++ sites/aquamagazine.com/config/identity-x.js
@@ -3,7 +3,7 @@ const configureIdentityX = require('@ab-media/package-global/config/identity-x')
 module.exports = configureIdentityX({
   appId: '6154db4dfa82c643bb70be4d',
   gtmUserFields: {
-    primary_role: '62b3088a6ca60666cb07dc79',
+    primary_business: '62b3088a6ca60666cb07dc79',
     job_title: '62b309776ca606cf3407dc85',
   },
 });
